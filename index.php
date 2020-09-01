874d454a51ff06a8b4093e21fa7f29d697b35507
rajkumarwebdeveloper <70141884+rajkumarwebdeveloper@users.noreply.github.com>
2020-09-01 21:08:50 +0530

    Revert "Update index.php"

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php 
+if(1==1) 
+echo "Final Pull request checking ";
+echo "Final Pull request checking asdasdasd
+$a=5;
+$a=+5;
 
+$a+=5;$a+=5;$a+=5;
+
+$a+=5;
+$a+=5;$a+=5;
 $a+=5;$a+=5;
 $s=0;
+
